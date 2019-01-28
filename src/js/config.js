a574f2633e3363748e32b1c665b12cd1a350967c
cmsio <mansio1102@gmail.com>
2019-01-27 23:46:34 -0800

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
 export const proxy = "https://cors-anywhere.herokuapp.com/";
-export const key = "da07beb1527fc5a305ed0862c19cc2d1";
-export const url = "http://food2fork.com/api/";
+export const key = "da07beb1527fc5a30";
+export const url = "http://food2fo";
